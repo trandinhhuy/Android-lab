488e2733f0dfa811dbf001f1c3ff960c24ae5db8
Huy <dinhuy1392000@gmail.com>
2021-03-09 00:07:45 +0700

    Flash screen

---
+++ app/src/main/java/com/example/everhope/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.everhope;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+    private static int Splash_time_out = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, Splash_time_out);
+    }
+}
