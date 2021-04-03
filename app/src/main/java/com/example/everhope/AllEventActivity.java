316bb7f108eecd135d59654a35917d3366e3f974
Huy <dinhuy1392000@gmail.com>
2021-04-03 22:56:46 +0700

    pull

---
+++ app/src/main/java/com/example/everhope/AllEventActivity.java
@@ -0,0 +1,14 @@
+package com.example.everhope;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_event);
+    }
+}
